6e10caec51148374e5d44a76214596eea1a86683
Zensa4060 <yagyamishra56@gmail.com>
2026-03-22 11:30:57 +0530

    update init

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,1 +1,19 @@
-﻿
+﻿from .auth import router as auth_router
+from .bot import router as bot_router
+from .game import router as game_router
+from .otp import router as otp_router
+from .paypal import router as paypal_router
+from .profile import router as profile_router
+from .room import router as room_router
+from .store import router as store_router
+
+__all__ = [
+    'auth_router',
+    'bot_router', 
+    'game_router',
+    'otp_router',
+    'paypal_router',
+    'profile_router',
+    'room_router',
+    'store_router'
+]
