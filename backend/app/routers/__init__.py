f59bb5c85aa7e6a527359a7cec7f1dcab7d8cd92
Zensa4060 <yagyamishra56@gmail.com>
2026-03-22 13:39:04 +0530

    try 7

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,10 +1,10 @@
-﻿
+
 # app/routers/__init__.py
 from .auth import router as auth
 from .bot import router as bot
 from .game import router as game
 from .otp import router as otp
-from .paypal import router as paypal
+from .paypal_service import router as paypal
 from .profile import router as profile
 from .room import router as room
 from .store import router as store
